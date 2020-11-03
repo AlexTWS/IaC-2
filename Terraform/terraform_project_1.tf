c7fa9c86054b6a4966455d12fad201a3f687e7e3
Khalid <kmoj86@gmail.com>
2020-11-03 21:24:00 +0300

    added tages

--- Terraform/terraform_project_1.tf
+++ Terraform/terraform_project_1.tf
@@ -16,4 +16,8 @@ resource "aws_instance" "example" {
     ami = "ami-00a205cb8e06c3c4e"
     key_name = "ssh_key_desktop"
     instance_type = "t2.micro"
+    tags = {
+        "Name": "minion"
+        project: "terraform"
+    }
 }
