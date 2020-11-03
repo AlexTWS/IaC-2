5732e104c699d483da136f94a3705d5e42887422
Khalid <kmoj86@gmail.com>
2020-11-03 09:52:05 +0300

    created  first terrafrom file

---
+++ Terraform/terraform_project_1.tf
@@ -0,0 +1,18 @@
+terraform {
+    reqired_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 2.70"
+        }
+    }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-00a205cb8e06c3c4e"
+    instance_type = "t2.micro"
+}
