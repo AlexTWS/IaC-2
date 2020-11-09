4c02415528b1a8063e7e32762ae78969d44f6b40
Khalid <kmoj86@gmail.com>
2020-11-09 11:20:29 +0300

    added rtb association

--- Terraform/project_1/network.tf
+++ Terraform/project_1/network.tf
@@ -45,11 +45,16 @@ resource "aws_route_table" "stripes_public_rtb" {
     vpc_id = aws_vpc.stripes_vpc.id
 
     route {
-        cidr_block = "10.0.0.0/24"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.stripes_igw.id
     }
 
     tags = {
         Name: "stripes_public_rtb"
     }
+}
+
+resource "aws_route_table_association" "rtb_publicSubnet_association" {
+  subnet_id      = aws_subnet.stripes_subnet_public.id
+  route_table_id = aws_route_table.stripes_public_rtb.id
 }
