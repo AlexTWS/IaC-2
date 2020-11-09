1711eadb8ca5382abddd03c32d8eb6bf370ebb5d
Khalid <kmoj86@gmail.com>
2020-11-09 10:22:11 +0300

    removed file name from referenced resources

--- Terraform/project_1/subnets.tf
+++ Terraform/project_1/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "stripes_subnet_public" {
-    vpc_id = vpc.aws_vpc.stripes_vpc.id
+    vpc_id = aws_vpc.stripes_vpc.id
     cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "eu-central-1a"
@@ -10,7 +10,7 @@ resource "aws_subnet" "stripes_subnet_public" {
 }
 
 resource "aws_subnet" "stripes_subnet_private" {
-    vpc_id = vpc.aws_vpc.stripes_vpc.id
+    vpc_id = aws_vpc.stripes_vpc.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "false"
     availability_zone = "eu-central-1b"
