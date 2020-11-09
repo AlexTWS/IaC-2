544cbd228e979a8995ab024a25db918ac60214e4
Khalid <kmoj86@gmail.com>
2020-11-09 09:50:21 +0300

    fixed naming error on AZ for subnet "stripes_subnet_private"

--- Terraform/project_1/main.tf
+++ Terraform/project_1/main.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "stripes_subnet_private" {
     vpc_id = "${aws_vpc.stripes_vpc.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu_central_1b"
+    availability_zone = "eu-central-1b"
 
     tags = {
         Name: "stripes_subnet_private"
