1e0f1b8abac83645e3364a01cacb972a6b886ea0
Khalid <kmoj86@gmail.com>
2020-11-09 22:41:12 +0300

    formatted code

--- Terraform/project_1/main.tf
+++ Terraform/project_1/main.tf
@@ -1,77 +1,77 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 2.70"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
     }
+  }
 }
 
 provider "aws" {
-    profile = "default"
-    region = "eu-central-1"
+  profile = "default"
+  region  = "eu-central-1"
 }
 
 resource "aws_security_group" "web_app" {
-    name = "web_app"
-    description = "allow HTTP port"
-    vpc_id = aws_vpc.stripes_vpc.id
+  name        = "web_app"
+  description = "allow HTTP port"
+  vpc_id      = aws_vpc.stripes_vpc.id
 
-    ingress {
-        description = "Allow HTTP"
-        from_port = 80
-        to_port = 80
-        protocol  = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "web_app"
-    }
+  tags = {
+    Name = "web_app"
+  }
 }
 
 resource "aws_security_group" "ssh" {
-    name = "ssh"
-    description = "allow port 22"
-    vpc_id = aws_vpc.stripes_vpc.id
+  name        = "ssh"
+  description = "allow port 22"
+  vpc_id      = aws_vpc.stripes_vpc.id
 
-    ingress {
-        description = "Allow SSH"
-        from_port = 22
-        to_port = 22
-        protocol  = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "ssh"
-    }
+  tags = {
+    Name = "ssh"
+  }
 }
 
 resource "aws_instance" "web_app" {
-    ami = "ami-00a205cb8e06c3c4e"
-    key_name = "ssh_key_desktop"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.stripes_subnet_public.id
-    security_groups = [ aws_security_group.ssh.id, aws_security_group.web_app.id ]
-        tags = {
-        Name: "web_app"
-        project: "terraform"
-    }
-    user_data = file("webapp.sh")
-    depends_on = [aws_internet_gateway.stripes_igw]
+  ami             = "ami-00a205cb8e06c3c4e"
+  key_name        = "ssh_key_desktop"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.stripes_subnet_public.id
+  security_groups = [aws_security_group.ssh.id, aws_security_group.web_app.id]
+  tags = {
+    Name : "web_app"
+    project : "terraform"
+  }
+  user_data  = file("webapp.sh")
+  depends_on = [aws_internet_gateway.stripes_igw]
 }
--- Terraform/project_1/network.tf
+++ Terraform/project_1/network.tf
@@ -1,57 +1,57 @@
 ######## VPC ########
 
 resource "aws_vpc" "stripes_vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "true"
-    tags = {
-        Name: "stripes_vpc"
-    }
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name : "stripes_vpc"
+  }
 }
 
 ######## SUBNETS ########
 
 resource "aws_subnet" "stripes_subnet_public" {
-    vpc_id = aws_vpc.stripes_vpc.id
-    cidr_block = "10.0.0.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-central-1a"
+  vpc_id                  = aws_vpc.stripes_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "eu-central-1a"
 
-    tags = {
-        Name: "stripes_subnet_public"
-    }
+  tags = {
+    Name : "stripes_subnet_public"
+  }
 }
 
 resource "aws_subnet" "stripes_subnet_private" {
-    vpc_id = aws_vpc.stripes_vpc.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-central-1b"
+  vpc_id                  = aws_vpc.stripes_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "eu-central-1b"
 
-    tags = {
-        Name: "stripes_subnet_private"
-    }
+  tags = {
+    Name : "stripes_subnet_private"
+  }
 }
 
 ######## INTERNET GATEWAY ########
 
 resource "aws_internet_gateway" "stripes_igw" {
-    vpc_id = aws_vpc.stripes_vpc.id
+  vpc_id = aws_vpc.stripes_vpc.id
 }
 
 ######## ROUTE TABLES ########
 
 resource "aws_route_table" "stripes_rtb_public" {
-    vpc_id = aws_vpc.stripes_vpc.id
+  vpc_id = aws_vpc.stripes_vpc.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.stripes_igw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.stripes_igw.id
+  }
 
-    tags = {
-        Name: "stripes_rtb_public"
-    }
+  tags = {
+    Name : "stripes_rtb_public"
+  }
 }
 
 resource "aws_route_table_association" "rtb_to_public_subnet_association" {
