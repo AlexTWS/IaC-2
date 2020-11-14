50da005a7baf51d7df44db37c401cde022b25322
Khalid <kmoj86@gmail.com>
2020-11-14 23:18:03 +0300

    allowed SSH port in web SG

--- 3.two_tier_with_rds/security_groups.tf
+++ 3.two_tier_with_rds/security_groups.tf
@@ -17,6 +17,12 @@ resource "aws_security_group" "lb" {
 resource "aws_security_group" "web" {
   vpc_id = aws_vpc.vpc.id
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # NEEDS TO BE CHANGED TO A MORE CLOSED DOWN CIDR BLOCK WHEN IN PRODUCTION
+  }
+  ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
