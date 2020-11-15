73a129cead65aa10f20be57fc37350c780b06698
Khalid <kmoj86@gmail.com>
2020-11-15 23:08:58 +0300

    updated user_data script

--- 2.scalable_website/user_data/hugo_site.sh
+++ 2.scalable_website/user_data/hugo_site.sh
@@ -2,7 +2,26 @@
 apt update -y
 apt install nginx nodejs hugo -y
 
+# NGINX CONFIG
+systemctl start nginx
+systemctl enable nginx
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /home/ubuntu/welcome/public/;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}
+EOF
+systemctl restart nginx
+
 # HUGO SITE
+su ubuntu
+cd
 hugo new site welcome
 cd welcome/
 git init
@@ -11,9 +30,3 @@ echo 'theme = "hugo-PaperMod"' >> config.toml
 hugo new posts/hello-from-terraform.md
 echo "If you see this, everything is working just fine!" >> content/posts/hello-from-terraform.md
 hugo
-
-# NGINX CONFIG
-systemctl start nginx
-systemctl enable nginx
-cat "nginx_config.txt" | tee /etc/nginx/sites-available/default
-systemctl restart nginx
