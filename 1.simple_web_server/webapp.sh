3ea088e8135a3f2a8a4228e5ee1ce0887e0508bc
Khalid <kmoj86@gmail.com>
2020-11-16 20:07:13 +0300

    updated webapp.sh

--- 1.simple_web_server/webapp.sh
+++ 1.simple_web_server/webapp.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Hello from Terraform</h1>" >> /var/www/html/index.html
+echo '<h1 style="text-align: center;">Hello from <span style="color: #623ce4;">Terraform</span>!</h1>' >> /var/www/html/index.html
