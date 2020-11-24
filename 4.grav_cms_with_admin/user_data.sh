3348ecfff7d3a11c9b4ecda5862eafbda4b4e43a
Khalid <kmoj86@gmail.com>
2020-11-25 00:12:58 +0300

    updated user data

--- 4.grav_cms_with_admin/user_data.sh
+++ 4.grav_cms_with_admin/user_data.sh
@@ -4,9 +4,12 @@ apt -y update
 apt -y install php #installs PHP and Apache web server
 apt -y install zip unzip
 
-https://getgrav.org/download/core/grav-admin/latest #download latest release of Grav with admin panel
-unzip latest -d /var/www/html/ #unzip folder into webroot
-cd /var/www/html/grav-admin/
+rm /var/www/html/index.html #remove default index.html
+wget https://getgrav.org/download/core/grav-admin/latest #download latest release of Grav with admin panel
+unzip latest
+cp -r grav-admin/. /var/www/html #copy all files and folders including hidden ones into webroot
+cd /var/www/html/
 bin/gpm install quark #install qaurk theme
+chown -R www-data:www-data /var/www/html/.
 
-sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/apache2.conf
+sed -i '/<Directory "\/var\/www\/html">/,/<\/Directory>/ s/AllowOverride None/AllowOverride all/' /etc/apache2/apache2.conf #change AllowOverride into All for .htaccess file
